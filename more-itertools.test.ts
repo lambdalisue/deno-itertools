eb1dc4a8cd98fd6b1e205e0258944ca1caba8437
Alisue <lambdalisue@gmail.com>
2023-05-08 09:42:56 +0900

    Use official BDD modules and upgrade deps

--- more-itertools.test.ts
+++ more-itertools.test.ts
@@ -1,4 +1,5 @@
-import { describe, expect, it } from "./deps_test.ts";
+import { describe, it } from "https://deno.land/std@0.186.0/testing/bdd.ts";
+import { expect } from "https://deno.land/x/expect@v0.3.0/mod.ts";
 import { range } from "./builtins.ts";
 import { first } from "./custom.ts";
 import {
