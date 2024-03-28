77d8959c714885be25107deb8e6253a1abe51d4d
lambdalisue <lambdalisue@users.noreply.github.com>
2024-03-28 00:16:01 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/deno-itertools/deno-itertools/custom.ts
    
    /home/runner/work/deno-itertools/deno-itertools/itertools.test.ts
    [1/2] Looking for releases: https://deno.land/std@0.186.0/testing/bdd.ts
    [1/2] Attempting update: https://deno.land/std@0.186.0/testing/bdd.ts -> 0.221.0
    [1/2] Update successful: https://deno.land/std@0.186.0/testing/bdd.ts -> 0.221.0
    [2/2] Looking for releases: https://deno.land/x/expect@v0.3.0/mod.ts
    [2/2] Attempting update: https://deno.land/x/expect@v0.3.0/mod.ts -> v0.4.0
    [2/2] Update successful: https://deno.land/x/expect@v0.3.0/mod.ts -> v0.4.0
    
    /home/runner/work/deno-itertools/deno-itertools/types.ts
    
    /home/runner/work/deno-itertools/deno-itertools/mod.ts
    
    /home/runner/work/deno-itertools/deno-itertools/builtins.ts
    
    /home/runner/work/deno-itertools/deno-itertools/utils.ts
    
    /home/runner/work/deno-itertools/deno-itertools/more-itertools.test.ts
    [1/2] Looking for releases: https://deno.land/std@0.186.0/testing/bdd.ts
    [1/2] Attempting update: https://deno.land/std@0.186.0/testing/bdd.ts -> 0.221.0
    [1/2] Update successful: https://deno.land/std@0.186.0/testing/bdd.ts -> 0.221.0
    [2/2] Looking for releases: https://deno.land/x/expect@v0.3.0/mod.ts
    [2/2] Attempting update: https://deno.land/x/expect@v0.3.0/mod.ts -> v0.4.0
    [2/2] Update successful: https://deno.land/x/expect@v0.3.0/mod.ts -> v0.4.0
    
    /home/runner/work/deno-itertools/deno-itertools/builtins.test.ts
    [1/3] Looking for releases: https://deno.land/std@0.186.0/testing/asserts.ts
    [1/3] Attempting update: https://deno.land/std@0.186.0/testing/asserts.ts -> 0.221.0
    [1/3] Update successful: https://deno.land/std@0.186.0/testing/asserts.ts -> 0.221.0
    [2/3] Looking for releases: https://deno.land/std@0.186.0/testing/bdd.ts
    [2/3] Attempting update: https://deno.land/std@0.186.0/testing/bdd.ts -> 0.221.0
    [2/3] Update successful: https://deno.land/std@0.186.0/testing/bdd.ts -> 0.221.0
    [3/3] Looking for releases: https://deno.land/x/expect@v0.3.0/mod.ts
    [3/3] Attempting update: https://deno.land/x/expect@v0.3.0/mod.ts -> v0.4.0
    [3/3] Update successful: https://deno.land/x/expect@v0.3.0/mod.ts -> v0.4.0
    
    /home/runner/work/deno-itertools/deno-itertools/itertools.ts
    
    /home/runner/work/deno-itertools/deno-itertools/more-itertools.ts
    
    /home/runner/work/deno-itertools/deno-itertools/custom.test.ts
    [1/2] Looking for releases: https://deno.land/std@0.186.0/testing/bdd.ts
    [1/2] Attempting update: https://deno.land/std@0.186.0/testing/bdd.ts -> 0.221.0
    [1/2] Update successful: https://deno.land/std@0.186.0/testing/bdd.ts -> 0.221.0
    [2/2] Looking for releases: https://deno.land/x/expect@v0.3.0/mod.ts
    [2/2] Attempting update: https://deno.land/x/expect@v0.3.0/mod.ts -> v0.4.0
    [2/2] Update successful: https://deno.land/x/expect@v0.3.0/mod.ts -> v0.4.0
    
    Successfully updated:
    https://deno.land/std@0.186.0/testing/bdd.ts 0.186.0 -> 0.221.0
    https://deno.land/x/expect@v0.3.0/mod.ts v0.3.0 -> v0.4.0
    https://deno.land/std@0.186.0/testing/bdd.ts 0.186.0 -> 0.221.0
    https://deno.land/x/expect@v0.3.0/mod.ts v0.3.0 -> v0.4.0
    https://deno.land/std@0.186.0/testing/asserts.ts 0.186.0 -> 0.221.0
    https://deno.land/std@0.186.0/testing/bdd.ts 0.186.0 -> 0.221.0
    https://deno.land/x/expect@v0.3.0/mod.ts v0.3.0 -> v0.4.0
    https://deno.land/std@0.186.0/testing/bdd.ts 0.186.0 -> 0.221.0
    https://deno.land/x/expect@v0.3.0/mod.ts v0.3.0 -> v0.4.0

--- custom.test.ts
+++ custom.test.ts
@@ -1,5 +1,5 @@
-import { describe, it } from "https://deno.land/std@0.186.0/testing/bdd.ts";
-import { expect } from "https://deno.land/x/expect@v0.3.0/mod.ts";
+import { describe, it } from "https://deno.land/std@0.221.0/testing/bdd.ts";
+import { expect } from "https://deno.land/x/expect@v0.4.0/mod.ts";
 import { compact, compactObject, flatmap } from "./custom.ts";
 import { repeat } from "./itertools.ts";
 
